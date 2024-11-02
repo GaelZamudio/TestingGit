ae08e6bd49e67b660ea908ac95799d87601780ae
Gael <zamudio.monroy.gael.armando@gmail.com>
2024-11-02 11:38:16 -0600

    Changing the title of the SignUp

--- src/main/java/com/testing/testinggit/App.java
+++ src/main/java/com/testing/testinggit/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("signUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 560, 560);
-        stage.setTitle("Echoband - Sign Up");
+        stage.setTitle("Echoband - Sign Up ");
         stage.setScene(scene);
         stage.show();
     }
