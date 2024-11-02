961e773cc0951c16c6f423e67399312bbd86e876
Gael <zamudio.monroy.gael.armando@gmail.com>
2024-11-02 10:06:55 -0600

    Adding Controller Files

--- src/main/java/com/testing/testinggit/controller/SignUpController.java
+++ src/main/java/com/testing/testinggit/controller/SignUpController.java
@@ -3,7 +3,7 @@ package com.testing.testinggit.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SignUpController {
     @FXML
     private Label welcomeText;
 
---
+++ src/main/java/com/testing/testinggit/dbconnection/Connection.java
@@ -0,0 +1,4 @@
+package com.testing.testinggit.dbconnection;
+
+public class Connection {
+}
